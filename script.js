0cd532b27e490c0036a1792007ca7e0a84a1a6ce
nightowL821 <101846848+nightowL821@users.noreply.github.com>
2024-11-13 22:00:04 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+function checkName() {
+    const name = document.getElementById('name').value.toLowerCase();
+    const validNames = ["mrittika", "tanbin rubayra mrittika", "babesh"];
+    
+    if (validNames.includes(name)) {
+        document.getElementById('question1').style.display = 'none';
+        document.getElementById('question2').style.display = 'block';
+    } else {
+        document.getElementById('question1').style.display = 'none';
+        document.getElementById('incorrectPage').style.display = 'block';
+    }
+}
+
+function goToQuestion3() {
+    document.getElementById('question2').style.display = 'none';
+    document.getElementById('question3').style.display = 'block';
+}
+
+function goToQuestion4() {
+    document.getElementById('question3').style.display = 'none';
+    document.getElementById('question4').style.display = 'block';
+}
+
+function goToQuestion5() {
+    document.getElementById('question4').style.display = 'none';
+    document.getElementById('question5').style.display = 'block';
+}
+
+function goToQuestion6() {
+    document.getElementById('question5').style.display = 'none';
+    document.getElementById('question6').style.display = 'block';
+}
+
+function finishDate() {
+    alert("Date scheduled!");
+}
+
+function resetPage() {
+    location.reload();
+}
